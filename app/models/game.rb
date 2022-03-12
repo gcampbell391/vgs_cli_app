18d594745045d749d9cd9325b0398ca6b014ce89
Gene Campbell <gcampbell391@gmail.com>
2022-03-12 16:20:38 -0500

    most of game is functional

--- app/models/game.rb
+++ app/models/game.rb
@@ -61,22 +61,23 @@ class Game < ApplicationRecord
     return currentDeck, playersCurrentScore, dealersFirstCardValue
   end
 
-  # Player continues to play until he or she chooses "Stay" or score is >= 21.
+  # Player continues to play until he or she chooses "Stay" or score is >= 21. 
   def hitOrStay(playerScore, currentDeck)
     prompt = TTY::Prompt.new
     playerChoseStay = false
     choices = {"Hit" => 1, "Stay" => 2}
+    updatedPlayerScore = playerScore
     while playerChoseStay == false 
       playerAction = prompt.select("Would You Like To Hit Or Stay?", choices)
       if playerAction == 2
         playerChoseStay = true
-        return playerScore, currentDeck
+        return updatedPlayerScore, currentDeck
       elsif playerAction == 1
         nextCard = currentDeck.shift
-        updatedPlayerScore = nextCard.card_value + playerScore
+        updatedPlayerScore = nextCard.card_value + updatedPlayerScore
         puts ""
         puts "#{self.dealer.name}: You chose hit. Your next card is #{nextCard.card_name}"
-        puts "#{self.dealer.name}: You're current score is #{updatedPlayerScore}"
+        puts "#{self.dealer.name}: Your current score is #{updatedPlayerScore}"
         puts ""
         if updatedPlayerScore >= 21
           return updatedPlayerScore, currentDeck
@@ -84,5 +85,79 @@ class Game < ApplicationRecord
       end
     end
   end
+
+  # Dealer's turn. 
+  def dealersTurn(playerScore, dealerScore, currentDeck)
+    puts ""
+    puts "Current Score:"
+    puts "#{self.player.name}: #{playerScore}"
+    puts "#{self.dealer.name}: #{dealerScore}"
+    puts ""
+    #Reveal dealer's second card.
+    puts "#{self.dealer.name}: I will now reveal my second card."
+    dealersSecondCard = currentDeck.shift
+    updatedDealerScore = dealerScore + dealersSecondCard.card_value
+    puts "#{self.dealer.name}: My second card is a #{dealersSecondCard.card_name}"
+    puts "#{self.dealer.name}: My current score is #{updatedDealerScore}"
+    puts ""
+    
+    # Dealer will continue to hit until score is > 17
+    while updatedDealerScore < 17 
+      dealersNextCard = currentDeck.shift
+      updatedDealerScore = updatedDealerScore+ dealersNextCard.card_value
+      puts "#{self.dealer.name}: I Am Going To Hit."
+      puts "#{self.dealer.name}: My next card is a #{dealersNextCard.card_name}"
+      puts "#{self.dealer.name}: My current score is #{updatedDealerScore}"
+      puts ""
+      puts "Current Score:"
+      puts "#{self.player.name}: #{playerScore}"
+      puts "#{self.dealer.name}: #{updatedDealerScore}"
+      puts ""
+    end
+    # Check if dealer has blackjack
+    if updatedDealerScore == 21
+      puts "#{self.dealer.name}: It's My Lucky Day! I Hit BlackJack!"
+      puts "#{self.dealer.name}: Tough break #{self.player.name}. Maybe Next Time."
+      puts ""
+      self.result = "Dealer Hit BlackJack! Dealer Won."
+      self.save
+      exit
+    # Check if dealer busted
+    elsif updatedDealerScore > 21
+      puts "#{self.dealer.name}: Ah Snap. I Busted.."
+      puts "#{self.dealer.name}: You Win This Time, But I Shall Win Next Time!"
+      puts ""
+      self.result = "Dealer Busted. Player Won."
+      self.save
+      exit
+    # Check if dealer scored at least 17. Dealer only hits if score is below 17.
+    elsif updatedDealerScore >= 17
+      if updatedDealerScore == playerScore
+        puts "#{self.dealer.name}: I'm Staying at #{updatedDealerScore}. Looks Like We Tie!"
+        puts "#{self.dealer.name}: You Thought You Were Going To Win With #{playerScore} Didn't You?"
+        puts ""
+        self.result = "Dealer Stayed At #{updatedDealerScore}, And Player Stayed At #{playerScore}. Game Was Tied."
+        self.save
+      # Check if dealer's score is less than players score after both players stayed
+      elsif updatedDealerScore < playerScore
+        puts "#{self.dealer.name}: I'm Staying at #{updatedDealerScore}. Looks Like You Win."
+        puts "#{self.dealer.name}: Are You Counting Cards? I'm On To You :)"
+        puts ""
+        self.result = "Dealer Stayed At #{updatedDealerScore}, And Player Stayed At #{playerScore}. Player Won."
+        self.save
+      # Check if dealer's score is more than players score after both players stayed
+      elsif updatedDealerScore > playerScore
+        puts "#{self.dealer.name}: I'm Staying at #{updatedDealerScore}. Looks Like I Win!"
+        puts "#{self.dealer.name}: Try Being More Aggresive Next Time!"
+        puts ""
+        self.result = "Dealer Stayed At #{updatedDealerScore}, And Player Stayed At #{playerScore}. Dealer Won."
+        self.save
+      end
+      exit
+    end
+    
+  end
+
+
 
 end
--- bin/cli.rb
+++ bin/cli.rb
@@ -17,6 +17,8 @@ if startGame
         puts "#{currentGame.dealer.name}: You Hit BlackJack! Congratulations! You Win!"
         puts "#{currentGame.dealer.name}: Please Come Back And Play Again!"
         puts ""
+        currentGame.result = "Player hit BlackJack! Player Won."
+        currentGame.save
         exit
     end 
     #Ask Player for turn. The Player continues until they select "Stay", hit BlackJack, or bust
@@ -26,16 +28,19 @@ if startGame
         puts "#{currentGame.dealer.name}: You Hit BlackJack! Congratulations! You Win!"
         puts "#{currentGame.dealer.name}: Please Come Back And Play Again!"
         puts ""
+        currentGame.result = "Player hit BlackJack! Player Won."
+        currentGame.save
         exit
     elsif currentPlayerScore > 21
         puts "#{currentGame.dealer.name}: I'm Sorry, But It Looks Like You Busted! I am Victorious!"
         puts "#{currentGame.dealer.name}: Better Luck Next Time, and Thanks for Playing!"
         puts ""
+        currentGame.result = "Player Busted. Dealer Won."
+        currentGame.save
         exit
     end 
-    
-
-
+    #Proceed with Dealers turn. Dealer reveals second card, and continues to hit if score is < 17
+    currentGame.dealersTurn(currentPlayerScore, currentDealerScore, currentDeck)
 else
     puts ""
     puts "Maybe Next Time!"
